a1891183ae6e1d1c47b50a75c397f80c3fe397d6
nightingalephillip <72528306+nightingalephillip@users.noreply.github.com>
2026-01-09 19:47:16 -0600

    Delete backend directory

--- backend/app/modules/auth/__init__.py
+++
@@ -1,5 +0,0 @@
-from .router import router
-from .models import User
-from .schemas import UserCreate, UserLogin, UserResponse, TokenResponse
-
-__all__ = ["router", "User", "UserCreate", "UserLogin", "UserResponse", "TokenResponse"]
